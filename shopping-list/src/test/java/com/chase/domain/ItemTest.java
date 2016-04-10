6015d597386f18be25e9e2ed9b5d16bcfcda1b2d
ChaseAgulhas <chase.agulhas@gmail.com>
2016-04-10 23:53:53 +0200

    Working on testing

---
+++ shopping-list/src/test/java/com/chase/domain/ItemTest.java
@@ -0,0 +1,18 @@
+package com.chase.domain;
+
+import com.chase.services.ItemInterface;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by student on 2016/04/10.
+ */
+public class ItemTest {
+
+    private ItemInterface item;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+
+    }
+}
